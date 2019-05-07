733e89b479739433ccfd5c5f7c78aa25a2ab9ced
KremerPereira <KremerPereira@KremerPereira-Notebook>
2019-05-06 20:26:36 -0400

    Em 06/05/2019. Criação e mapeamento dos atributos de classe (Pessoa e Cliente) e Hibernate Config

--- src/java/br/com/locadora/mapeamento/pessoa/Pessoa.java
+++ src/java/br/com/locadora/mapeamento/pessoa/Pessoa.java
@@ -5,13 +5,62 @@
  */
 package br.com.locadora.mapeamento.pessoa;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  *
  * @author KremerPereira
  */
+// Para mapear as Super Classes
+@MappedSuperclass
 public abstract class Pessoa {
     
+    @Id
+    @GeneratedValue
+    private long id;
+    // limitando o tamanho do campo e deixando desabilitando NULO, ou seja, não poderá ser NULO
+    @Column(length = 100, nullable = false )
     private String nome;
+    
+    @Column(nullable = false)
     private int idade;
-    private String cpf;
+    
+    // Campo que poderá receber string tanto de CPF quanto de CNPJ e também não poderá ser NULO
+    @Column(length = 15, nullable = false)
+    private String cpfCNPJ;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpfCNPJ() {
+        return cpfCNPJ;
+    }
+
+    public void setCpfCNPJ(String cpfCNPJ) {
+        this.cpfCNPJ = cpfCNPJ;
+    }
 }
